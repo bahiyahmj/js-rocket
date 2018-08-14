89ca25360daab38792919f6b8b5e39f118d77609
bahiyahmj <41480208+bahiyahmj@users.noreply.github.com>
2018-08-14 16:53:23 +0800

    made our cool rocket with drawing

--- js/rocket.js
+++ js/rocket.js
@@ -14,9 +14,23 @@ var changeState = function (state) {
 			countdownNumber = countdownNumber -1;
 			document.getElementById('countdown').innerHTML = countdownNumber;
 			
+			if (countdownNumber > 4 && countdownNumber <= 7) {
+				// be nervous
+				document.getElementById('nervous').className = 'nervous show';
+			} else{
+				document.getElementById('nervous').className = 'nervous';
+			}
+
+			if (countdownNumber > 1 && countdownNumber <= 4) {
+				// can't wait
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else{
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
+
 			if (countdownNumber <=0) {
 				changeState(3);
-			}
+			};
 		}, 500);
 	} else if (state == 3) {
 		var success = timer = setTimeout(function ()
